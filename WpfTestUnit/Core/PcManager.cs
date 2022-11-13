b8447f5eada0cde7c056602aabd66facf7f3bce2
bumboks21 <113061095+bumboks21@users.noreply.github.com>
2022-11-13 23:29:52 +0300

    WpfTestHarchenkoIS32

---
+++ WpfTestUnit/Core/PcManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestUnit.Core
+{
+    public class PcManager
+    {
+        public string LoadPcFeature(int numberOfCpus, bool os64)
+        {
+            try
+            {
+                if (numberOfCpus == 0)
+                {
+                    return "The number of CPUs cannot be zero";
+                }
+
+
+                BasicInfo myInfo = new BasicInfo
+                {
+                    OsVersion = Environment.OSVersion.ToString(),
+                    Os64 = os64,
+                    PcName = Environment.MachineName,
+                    NumberOfCpus = numberOfCpus,
+                    WindowsFolder = Environment.SystemDirectory,
+                    LogicalDrives = Environment.GetLogicalDrives()
+                };
+
+                return "PC data has been successfully generated";
+            }
+            catch
+            {
+
+                return "An error occurred during data generation";
+            }
+
+        }
+    }
+}
---
+++ WpfTestUnitTests/Core/PcManagerTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfTestUnit.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestUnit.Core.Tests
+{
+    [TestClass()]
+    public class PcManagerTests
+    {
+        PcManager pcManager = new PcManager();
+        [TestMethod()]
+        public void LoadPcFeatureTest()
+        {
+            //Allert 
+            int numberOfCups = 0;
+            bool os64 = false;
+            string expected = "The number of CPUs cannot be zero";
+            //Act 
+            var result = pcManager.LoadPcFeature(numberOfCups, os64);
+            //Assert 
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void LoadPcFeatureTest_Negative()
+        {
+            //Allert 
+            int numberOfCups = -1;
+            bool os64 = true;
+            string expected = "An error occurred during data generation";
+            //Act 
+            var result = pcManager.LoadPcFeature(numberOfCups, os64);
+            //Assert 
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void LoadPcFeatureTest_Positive()
+        {
+            //Allert 
+            int numberOfCups = 1;
+            bool os64 = true;
+            string expected = "PC data has been successfully generated";
+            //Act 
+            var result = pcManager.LoadPcFeature(numberOfCups, os64);
+            //Assert 
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void LoadPcFeatureTest_OS64()
+        {
+            //Allert 
+            int numberOfCups = 19879;
+            bool os64 = true;
+            string expected = "PC data has been successfully generated";
+            //Act 
+            var result = pcManager.LoadPcFeature(numberOfCups, os64);
+            //Assert 
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void LoadPcFeatureTest_LongValue_NotLive()
+        {
+            //Allert 
+            int numberOfCups = 666666;
+            bool os64 = false;
+            string expected = "An error occurred during data generation";
+            //Act 
+            var result = pcManager.LoadPcFeature(numberOfCups, os64);
+            //Assert 
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+    
